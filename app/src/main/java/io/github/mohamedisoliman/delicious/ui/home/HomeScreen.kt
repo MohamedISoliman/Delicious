d8e7758af10e6c12cbd361e501f6e8e44f405964
Mohamed Ibrahim <mibrahim.dev@gmail.com>
2021-11-17 21:06:37 +0200

    Implement Search Topbar

--- app/src/main/java/io/github/mohamedisoliman/delicious/ui/home/HomeScreen.kt
+++ app/src/main/java/io/github/mohamedisoliman/delicious/ui/home/HomeScreen.kt
@@ -2,23 +2,24 @@ package io.github.mohamedisoliman.delicious.ui.home
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Divider
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material.icons.outlined.Warning
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import io.github.mohamedisoliman.delicious.R
 import io.github.mohamedisoliman.delicious.domain.entities.Restaurant
 import io.github.mohamedisoliman.delicious.domain.restaurants.HomeViewState
 import io.github.mohamedisoliman.delicious.ui.common.PlaceHolderView
-import io.github.mohamedisoliman.delicious.R
 
 
 @Composable
@@ -35,17 +36,59 @@ private fun HomeContent(
     state: HomeViewState = HomeViewState.Idle,
     onItemClicked: (String) -> Unit = { },
 ) {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(horizontal = 16.dp),
-        contentAlignment = Alignment.Center
-    ) {
-        LoadingView(modifier = Modifier.align(Alignment.Center), isLoading = state.isLoading)
-        ListView(list = state.restaurants, onItemClicked = onItemClicked)
-        EmptyView(list = state.restaurants)
-        ErrorView(throwable = state.throwable)
+    Column {
+
+        SearchTopBar()
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 16.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            LoadingView(modifier = Modifier.align(Alignment.Center), isLoading = state.isLoading)
+            ListView(list = state.restaurants, onItemClicked = onItemClicked)
+            EmptyView(list = state.restaurants)
+            ErrorView(throwable = state.throwable)
+        }
+
     }
+
+}
+
+
+@Composable
+fun SearchTopBar(
+    modifier: Modifier = Modifier,
+    searchText: String = "",
+    onSearchChange: (String) -> Unit = {},
+    onSearchClicked: () -> Unit = {},
+) {
+    TextField(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp, horizontal = 8.dp),
+        value = searchText,
+        colors = TextFieldDefaults.textFieldColors(
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
+        ),
+        onValueChange = {
+            onSearchChange(it)
+        },
+        shape = RoundedCornerShape(8.dp),
+        trailingIcon = {
+            IconButton(onClick = { onSearchClicked() }) {
+                Icon(
+                    imageVector = Icons.Outlined.Search,
+                    tint = MaterialTheme.colors.onSurface,
+                    contentDescription = ""
+                )
+            }
+        },
+        singleLine = true,
+        keyboardActions = KeyboardActions(onDone = { onSearchClicked() })
+    )
 }
 
 @Composable
